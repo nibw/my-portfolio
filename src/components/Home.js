fd2bbe29672574024ab17ebe52daa888d2a6744b
nibw <nur.ismam13@gmail.com>
2021-05-26 00:31:03 +0700

    personalize home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
+import image from "../img/awp05_alltolls_blogspot092-1280x1024.jpg";
 
 export default function Home() {
-    return <h1>Home Page!</h1>
+    return (
+        <main>
+            <img src={image} alt="Keyboard grass" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-made">Halo!!!</h1>
+            </section>
+        </main>
+    )
 }
